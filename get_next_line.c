7fe73d059a198dead082166d97f63c8f71b9ca30
Edieaoe <edieaoe@gmail.com>
2024-06-16 11:00:12 +0900

    .

--- get_next_line.c
+++ get_next_line.c
@@ -3,37 +3,80 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yiliu <yiliu@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*   By: yxu <yxu@student.42tokyo.jp>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 16:22:55 by yiliu             #+#    #+#             */
-/*   Updated: 2024/05/11 18:31:32 by yiliu            ###   ########.fr       */
+/*   Updated: 2024/06/15 23:07:12 by yxu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+char	*ft_read(int fd, char *save)
 {
-	char	*result;
-	char	**buffer;
-	int		maximumlinelength;
-	int		bytesread;
+	char	*buffer;
+	int		readbyte;
+
+	buffer = (char *)malloc((BUFF_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (NULL);
+	readbyte = 1;
+	while (readbyte != 0)
+	{
+		readbyte = read(fd, buffer, BUFF_SIZE);
+		if (readbyte == -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		buffer[readbyte] = '\0';
+		save = ft_strjoin(save, buffer);
+	}
+	free(buffer);
+	return (save);
+}
+
+char	*get_line(char *save)
+(
+	char	*line;
 	int		i;
 
-	maximumlinelength = 128;
-	buffer = (char **)malloc(maximumlinelength * sizeof(char *));
-	if (!buffer)
-
-	bytesread = read(fd, buffer, sizeof(buffer));
-	result = (char *)malloc(bytesread * sizeof(char));
-	if (!result)
-
 	i = 0;
-	while (i < bytesread)
-	{
-		result[i] == buffer[i];
+	while (!save[i] && save[i] != '\n')
 		i++;
-	}
-	free (buffer);
-	return (result);
+	line = (char *)malloc((i + 2) * sizeof(char))
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (!save[i] && save[i] != '\n')
+		line[i++] == save[i++];
+	line[i] = '\n';
+	line[i + 1] = '\0';
+	return (line);
+)
+
+char	*afterline(char *save)
+{
+	char	*line;
+	int		i;
+	int		n;
+
+	line = get_line(save);
+	i = ft_strlen(line) + 1;
+	n = 0;
+	while (!save[i])
+		save[n++] = save[i++];
+	save[n] = '\0';
+	return (save);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*save;
+
+	save = ft_read(fd, save);
+	line = get_line(save);
+	save = afterline(save);
+	return (line);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,10 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yiliu <yiliu@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*   By: yxu <yxu@student.42tokyo.jp>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 16:24:08 by yiliu             #+#    #+#             */
-/*   Updated: 2024/05/11 16:24:09 by yiliu            ###   ########.fr       */
+/*   Updated: 2024/06/15 23:08:06 by yxu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*result;
+	size_t	s1len;
+	size_t	s2len;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1len = ft_strlen(s1);
+	s2len = ft_strlen(s2);
+	result = (char *)malloc((s1len + s2len + 1) * sizeof(char));
+	if (!result)
+		return (0);
+	ft_memcpy(result, s1, s1len);
+	ft_memcpy(result + s1len, s2, s2len);
+	result[s1len + s2len] = '\0';
+	return (result);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (*s++)
+		len++;
+	return (len);
+}
